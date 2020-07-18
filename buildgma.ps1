40b41fa3af5bdb6d3c7d761ff7e23e52abea3739
Will <will@miats.com>
2020-07-18 14:02:07 -0700

    Auto convert taunt audio files on build

---
+++ buildgma.ps1
@@ -0,0 +1,13 @@
+Remove-Item -Path "build\necrotaunts.gma" -ErrorAction Ignore | Out-Null
+Remove-Item -Path "build\necrotaunts" -Recurse -ErrorAction Ignore | Out-Null
+New-Item -Path "build" -ItemType "directory" -ErrorAction Ignore | Out-Null
+New-Item -Path "build\necrotaunts\sound\taunts" -ItemType "directory" -ErrorAction Ignore | Out-Null
+
+Copy-Item "addon/*" "build\necrotaunts" -Recurse | Out-Null
+
+$files = Get-ChildItem taunts
+foreach ($file in $files) {
+    ./sox/sox.exe "$($file.FullName)" -c 1 -r 44100 "build\necrotaunts\sound\taunts\$($file.BaseName).wav"
+}
+
+C:\"Program Files (x86)"\Steam\steamapps\common\garrysmod\bin\gmad.exe create -folder "build\necrotaunts" -out "build\necrotaunts.gma"
