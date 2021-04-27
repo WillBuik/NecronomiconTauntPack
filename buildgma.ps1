d7115b903b3dcc67e23f6f58bfc534f4ac3f4716
Will <will@miats.com>
2021-04-26 22:56:05 -0700

    Switch taunt pack to use OGG Vorbis codec instead of PCM/WAV

--- buildgma.ps1
+++ buildgma.ps1
@@ -7,7 +7,11 @@ Copy-Item "addon/*" "build\necrotaunts" -Recurse | Out-Null
 
 $files = Get-ChildItem taunts
 foreach ($file in $files) {
-    ./sox/sox.exe "$($file.FullName)" -c 1 -r 44100 -b 16 --norm "build\necrotaunts\sound\taunts\$($file.BaseName).wav"
+    # WAV
+    ##./sox/sox.exe "$($file.FullName)" -c 1 -r 44100 -b 16 --norm "build\necrotaunts\sound\taunts\$($file.BaseName).wav"
+
+    # OGG
+    ./sox/sox.exe "$($file.FullName)" -C 6 -c 1 -r 44100 --norm "build\necrotaunts\sound\taunts\$($file.BaseName).ogg"
 }
 
 C:\"Program Files (x86)"\Steam\steamapps\common\garrysmod\bin\gmad.exe create -folder "build\necrotaunts" -out "build\necrotaunts.gma"
