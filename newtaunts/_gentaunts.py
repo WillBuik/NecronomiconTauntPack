33812606fd83c41ca4954d72362ea85c250ed105
Will <will@miats.com>
2021-05-16 16:39:51 -0700

    Add python script to simplify adding new taunts

---
+++ newtaunts/_gentaunts.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import re
+
+newtaunts = Path('.')
+
+for f in newtaunts.iterdir():
+    ext = f.suffix.lower()
+    if f.is_file() and (ext == ".wav" or ext == ".mp3" or ext == ".ogg"):
+
+        # Make name gmod compatible
+        newname = f.name.lower().replace(' ', '_').replace('-', '_')
+        newname = re.sub('_+', '_', newname)
+        newname = re.sub(r'[^a-z0-9_\.]', '', newname)
+        if f.name != newname:
+            f = f.rename(newname)
+
+        # Print addtaunts.lua code
+        displayname = f.stem.replace('_', ' ').title()
+        print(f'PROP_TAUNTS["{displayname}"]'.ljust(44, ' ') + f' = "{f.stem}.ogg"')
